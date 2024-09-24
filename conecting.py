7ba5530b22b64f9fc86d91185b8ba0aa948be9ac
kyleezy <kyleezywakabi@gmail.com>
2024-09-24 20:11:14 +0200

    Add files via upload

--- conecting.py
+++ conecting.py
@@ -1,12 +1,24 @@
 import pgzrun
+import random
 WIDTH=800
 HEIGHT=600
 TITLE='space'
+total=5
+satellites=[]
+for i in range(5):
+  space=Actor("st")
+  x=random.randint(50,650)
+  y=random.randint(50,550)
+  space.pos=x,y
+  satellites.append(space)
 def draw():
     #screen.clear()
     screen.fill('red')
+    for satellite in satellites:
+       satellite.draw() 
 def update():
     pass
+
 
 
 
