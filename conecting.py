d0924ffc77f9483303e4ed4a592f5ae73b44f15b
kyleezy <kyleezywakabi@gmail.com>
2024-10-01 20:20:07 +0200

    Add files via upload

--- conecting.py
+++ conecting.py
@@ -4,6 +4,8 @@ WIDTH=800
 HEIGHT=600
 TITLE='space'
 total=5
+target=1
+lines=[]
 satellites=[]
 for i in range(5):
   space=Actor("st")
@@ -14,8 +16,16 @@ for i in range(5):
 def draw():
     #screen.clear()
     screen.fill('red')
+    number=1
     for satellite in satellites:
        satellite.draw() 
+       screen.draw.text(str(number),(satellite.x,satellite.y+30))
+       number=number+1
+def on_mouse_down(pos):
+  if satellites[target-1].collidepoint(pos):
+    if target>1:
+      lines.append((satellites[target-2].pos,satellites[target-1].pos))
+
 def update():
     pass
 
