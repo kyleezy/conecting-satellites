2759071124c2d31a1f8da01447b80374f1ebebb3
kyleezy <kyleezywakabi@gmail.com>
2024-10-08 20:16:05 +0200

    Add files via upload

--- conecting.py
+++ conecting.py
@@ -4,6 +4,7 @@ WIDTH=800
 HEIGHT=600
 TITLE='space'
 total=5
+gameover=False
 target=1
 lines=[]
 satellites=[]
@@ -21,10 +22,22 @@ def draw():
        satellite.draw() 
        screen.draw.text(str(number),(satellite.x,satellite.y+30))
        number=number+1
+    for line in lines:
+      screen.draw.line(line[0],line[1],"blue")
+    if gameover==True:
+      screen.draw.text("gameover",(0,0))
 def on_mouse_down(pos):
-  if satellites[target-1].collidepoint(pos):
-    if target>1:
-      lines.append((satellites[target-2].pos,satellites[target-1].pos))
+  global target
+  global gameover
+  if target<6 and gameover==False:
+    if satellites[target-1].collidepoint(pos):
+      if target>1:
+        lines.append((satellites[target-2].pos,satellites[target-1].pos ))
+      target=target+1
+    else:
+      gameover=True 
+
+
 
 def update():
     pass
